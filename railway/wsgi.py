c95d309f33ba7993073849734d5fcceb478335a2
yash240408 <yash.mistry.g43@gmail.com>
2022-08-06 11:33:59 +0530

    Updated Required Things

--- railway/wsgi.py
+++ railway/wsgi.py
@@ -6,11 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'railway.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
